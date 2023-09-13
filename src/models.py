8f7f33cb4fb7a0d3ce8f9d23b9d05b7e04d5bb9c
Raul Gimenez Murga <136567985+Raauul1996@users.noreply.github.com>
2023-09-13 17:21:52 +0000

    making diagram logics

--- src/models.py
+++ src/models.py
@@ -1,29 +1,53 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Planet(Base):
+    __tablename__ = 'planet'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    gravity = Column(Float, nullable=False)
+    population = Column(Integer, nullable=False)
+    image = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
+# enumerate((0,"Male"),(1,"Female"),(2,"Other"))
+
+class Character(Base):
+    __tablename__ = 'character'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    specie = Column(String(250), nullable=False)
+    genre = Column(String(250), nullable=False)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planet)
+
+class Starship(Base):
+    __tablename__ = 'starship'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    model = Column(String(250), nullable=False)
+    name = Column(String(250))
+    image = Column(String(250))
+
+class Characters_Starships(Base):
+    __tablename__ = 'characters_starships'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    character = relationship(Character)
+    starship_id = Column(Integer, ForeignKey('starship.id'))
+    starship = relationship(Starship)
 
     def to_dict(self):
         return {}
